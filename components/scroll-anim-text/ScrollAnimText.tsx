a6e2dc4779ac308943391f62377f70fe38fa7d25
zakir <zakirhossaib736@gmail.com>
2022-10-22 15:26:05 +0600

    banner issue solved

--- components/scroll-anim-text/ScrollAnimText.tsx
+++ components/scroll-anim-text/ScrollAnimText.tsx
@@ -12,27 +12,28 @@ const ScrollAnimText: NextPage<any> = ({}) => {
     document.body.style.overflow = 'auto';
     document.scrollingElement.scrollTo(0, 0);
     
-    gsap.utils.toArray('.scrolable-text-box').forEach((section, index) => {
+    gsap.utils.toArray('.scroll-anim-text').forEach((section, index) => {
       const w = section.querySelector('.scroll-text-item');
-      const [x, xEnd] = (index % 2) ? ['200%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+      const [x, xEnd] = (index % 5) ? ['50%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
       gsap.fromTo(w, {  x  }, {
         x: xEnd,
         scrollTrigger: { 
           trigger: section, 
           scrub: 0.5 ,
+          start: 'top 170px',
+          end: 'bottom top+=170px',
         },
         y: 0,
         direction:10,
         
       });
     });
-
   });
 
   return (
     <>
-      <section className="scroll-anim-text  relative w-full h-full md:h-auto px-0 bg-primary overflow-hidden md:pt-8  sm:pt-6">
-        <div className="scrolable-bg-shape prallex bg-shape absolute top-0 left-0 w-full sm:h-full">
+      <section className="scroll-anim-text  relative w-full h-[456px] md:h-auto px-0 bg-primary  md:pt-8  sm:pt-6">
+        {/* <div className="scrolable-bg-shape prallex bg-shape absolute top-0 left-0 w-full sm:h-full">
           <img
             src="/images/scroll-text-bg.png"
             alt="superfandom bg-shape"
@@ -40,8 +41,8 @@ const ScrollAnimText: NextPage<any> = ({}) => {
             width="1920px"
             height="1080px"
           />
-        </div>
-        <div className="scroll-anim-text-wrapper pt-[60px] pb-[90px] w-full z-10 relative overflow-hidden">
+        </div> */}
+        <div className="scroll-anim-text-wrapper  w-full z-10 relative mt-[-150px]">
           <div className="section-titlebox mb-24">
             <div className="scrolable-text-box">
             <div className="scroll-text-item">
@@ -79,17 +80,7 @@ const ScrollAnimText: NextPage<any> = ({}) => {
             </div>
             </div>
 
-            <div className="scrolable-text-box">
-            <div className="scroll-text-item">
-            <h2 className="section-title whitespace-nowrap scrollable--title2 font-primary font-normal text-fig-5x text-center uppercase md:text-fig-xx sm:text-fig-32 mb-[29px] md:mb-[14px]">
-            soulbound • tokens • no • 
-            soulbound • tokens • no • 
-            soulbound • tokens • no • 
-            soulbound • tokens • no • 
-            soulbound • tokens • no • 
-            </h2>
-            </div>
-            </div>
+            
           </div>
         </div>
       </section>
