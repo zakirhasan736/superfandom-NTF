792928cb2a689c6078c73bbd691e19df7f1e4d62
Kazi Rayhan <rayhanbd4400@gmail.com>
2022-10-23 16:07:15 +0600

    fix all ts problem

--- components/accordion/Faq.tsx
+++ components/accordion/Faq.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
 export interface IFaq {
-  id: number;
-  title: string;
-  desc: string;
+  id: number,
+  question: string;
+  answer: string;
 }
 
-const Faq: React.FC<IFaq> = ({ id, title, desc }) => {
+const Faq: React.FC<IFaq> = ({id, question, answer }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
@@ -23,33 +23,30 @@ const Faq: React.FC<IFaq> = ({ id, title, desc }) => {
             <div className="col-sm-12 d-flex flex-column w-100 justify-content-center align-items-center px-0">
               <h5 className="font-32 color-black">FAQs</h5>
               <div className="w-100 cursor-pointer bg-light-gray mt-4">
+              <div
+            className="py-2 mb-4"
+            style={{ borderBottom: '1px solid #201627' }}
+            id={`faqs-accordion-${id}-wrapper`}
+        >
+            <div
+                className="d-flex flex-row justify-content-between align-items-center w-100"
+                onClick={() => setIsActive(!isActive)}
+                id={`faqs-accordion-${id}`}
+            >
+                <div className="font-24 color-black">{question}</div>
+                {isActive ? (
+                    <img src="/assets/icon/arrow-up.svg" alt={'arrow-up'} />
+                ) : (
+                    <img src="/assets/icon/arrow-down.svg" alt={'arrow-down'} />
+                )}
+            </div>
+            {isActive && (
                 <div
-                  className="py-2 mb-4"
-                  style={{ borderBottom: '1px solid #201627' }}
-                  id={`faqs-accordion-${id}-wrapper`}
-                >
-                  <div
-                    className="d-flex flex-row justify-content-between align-items-center w-100"
-                    onClick={() => setIsActive(!isActive)}
-                    id={`faqs-accordion-${id}`}
-                  >
-                    <div className="font-24 color-black">{title}</div>
-                    {isActive ? (
-                      <img src="/assets/icon/arrow-up.svg" alt={'arrow-up'} />
-                    ) : (
-                      <img
-                        src="/assets/icon/arrow-down.svg"
-                        alt={'arrow-down'}
-                      />
-                    )}
-                  </div>
-                  {isActive && (
-                    <div
-                      dangerouslySetInnerHTML={{ __html: desc }}
-                      className="text-left mt-3 font-20thin color-black"
-                    ></div>
-                  )}
-                </div>
+                    dangerouslySetInnerHTML={{ __html: answer }}
+                    className="text-left mt-3 font-20thin color-black"
+                ></div>
+            )}
+        </div>
               </div>
             </div>
           </div>
